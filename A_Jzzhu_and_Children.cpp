3dc4a529f1bfa41624c560d98e1e5d911594751e
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-14 23:50:11 +0530

    Deque

--- A_Jzzhu_and_Children.cpp
+++ A_Jzzhu_and_Children.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <deque>
 #include <string>
 using namespace std;
-void solve()
-{
-}
 int main()
 {
+    deque<pair<int, int>> qu;
+    int size, candies;
+    cin >> size >> candies;
+    for (int i = 1; i <= size; i++)
+    {
+        int temp;
+        pair<int, int> p;
+        p.first = i;
+        scanf("%d", &temp);
+        p.second = temp;
+        qu.push_back(p);
+    }
+    for (auto i : qu)
+    {
+        cout << i.first << ":" << i.second << endl;
+    }
     return 0;
 }
