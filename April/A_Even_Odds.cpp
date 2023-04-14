6a65d5f47fba4528b7e60753f5e379ec7ccc7641
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-04-15 02:24:49 +0530

    318A - Even Odds

---
+++ April/A_Even_Odds.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main()
+{
+    long long int range, k;
+    cin >> range >> k;
+    // Even Case & Odd case
+    if (range % 2 == 0)
+    {
+        if (k <= (range / 2))
+        {
+            cout << 1 + (k - 1) * 2;
+        }
+        else
+        {
+            cout << 2 + ((k - (range / 2)) - 1) * 2;
+        }
+    }
+    else
+    {
+        if (k <= (range / 2) + 1)
+        {
+            cout << 1 + (k - 1) * 2;
+        }
+        else
+        {
+            cout << 2 + (((k - 1) - (range / 2)) - 1) * 2;
+        }
+    }
+    return 0;
+}
