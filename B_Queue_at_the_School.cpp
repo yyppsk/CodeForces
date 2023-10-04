3e4014b858eb9ff9730bef47ee4f133bbee5225d
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-10-04 23:21:29 +0530

    B. Queue at the School

--- B_Queue_at_the_School.cpp
+++ B_Queue_at_the_School.cpp
@@ -2,20 +2,39 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+string solve(string &children, int time, int size)
+{
+    int c = 0;
+    while (c != time)
+    {
+        int head = 0;
+        int tail = 1;
+        for (int i = 0; i < children.size(); i++)
+        {
+            if (head >= size || tail >= size)
+                break;
+            if (children[head] == 'B' && children[tail] == 'G')
+            {
+                swap(children[head], children[tail]);
+                head += 2;
+                tail += 2;
+            }
+            else
+            {
+                head++;
+                tail++;
+            }
+        }
+        c++;
+    }
+    return children;
+}
 int main()
 {
-    int children, time;
-    vector<char> queue;
-    scanf("%d %d", &children, &time);
-    for (int i = 0; i <= children; i++)
-    {
-        char temp;
-        scanf("%c", &temp);
-        queue.push_back(temp);
-    }
-    for (auto i : queue)
-    {
-        printf("%c", i);
-    }
+    string children;
+    int time, n;
+    cin >> n >> time;
+    cin >> children;
+    cout << solve(children, time, n);
     return 0;
 }
